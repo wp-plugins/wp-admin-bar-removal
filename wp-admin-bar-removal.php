e14ea2e45a731985d3a918f5bf0ff3e65b0f221d
sLaNGjI <sLaNGjI@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-25 13:43:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-admin-bar-removal/tags/2011.0223.2011@510387 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-admin-bar-removal.php
+++ wp-admin-bar-removal.php
@@ -2,18 +2,18 @@
 /*
 Plugin Name: Admin Bar Removal (3.1+ only)
 Plugin URI: http://wordpress.org/extend/plugins/wp-admin-bar-removal/
-Description: Completely <code>Disable</code> new WordPress 3.1+ <code>Admin Bar</code> Frontend and Backend. Completely <code>Remove Code</code> for minimal memory load | <a href="http://donate.sla.lcsn.net/" title="Donate author plugin">Donate</a> | <a href="http://wordpress.org/extend/plugins/wp-overview-lite/" title="Show Dashboard Overview and Memory Load Usage on Footer">WP Overview?</a> | <a href="http://wordpress.org/extend/plugins/wp-missed-schedule/" title="Fix Missed Scheduled Future Posts Cron Job">WP Missed Schedule?</a>
+Description: Completely <code>Disable</code> new WordPress 3.1+ <code>Admin Bar</code> Frontend and Backend. Completely <code>Remove Code</code> for minimal memory load | <a href="http://lcsn.net/donate/" title="Free Donation">Donate</a> | <a href="http://wordpress.org/extend/plugins/wp-overview-lite/" title="Show Dashboard Overview and Memory Load Usage on Footer">WP Overview?</a> | <a href="http://wordpress.org/extend/plugins/wp-missed-schedule/" title="Fix Missed Scheduled Future Posts Cron Job">WP Missed Schedule?</a>
 Version: 2011.0223.2011
 Author: sLa
 Author URI: http://wordpress.org/extend/plugins/profile/sla/
 Requires at least: 3.1
 Tested up to: 3.2
-License: GPLv2 or later
+License: GPLv2
  * 
  * Development Release: Version 2011 Build 0423-BUGFIX Revision 0120-DEVELOPMENTAL
  * Stable Release: Version 2011 Build 0223 Revision 2011
  *
- *  Admin Bar Removal - WordPress PlugIn
+ * Admin Bar Removal - WordPress PlugIn
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the [GNU General Public License](http://wordpress.org/about/gpl/)
@@ -29,7 +29,7 @@ License: GPLv2 or later
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Part of Copyright © 2010-2011 belongs to sLaT ™ [LavaTeam] NGjI ™ [NewGenerationInterfaces] (slangji [at] gmail [dot] com)
+ * Part of Copyright © 2010-2011 belongs to slaT ™ [LavaTeam] NGjI ™ [NewGenerationInterfaces] (slangji [at] gmail [dot] com)
  * and a portion to their respective owners. Not For Resale or Business Purpose.
  */
 /**
@@ -37,6 +37,8 @@ License: GPLv2 or later
  * @subpackage WordPress PlugIn
  * @since 3.1.0
  * @version 2011.0223.2011
+ * @author sLa
+ * @license GPLv2
  *
  * Completely Disable Admin Bar Frontend, Backend and Remove Code, for minimal memory load.
  */
