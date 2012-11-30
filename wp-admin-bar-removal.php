2918d59959efbf4674d24bfb81ca085d5b625084
sLaNGjI <sLaNGjI@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-30 06:59:03 +0000

    Silent Update - Enanched Code
    git-svn-id: https://plugins.svn.wordpress.org/wp-admin-bar-removal/trunk@632125 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-admin-bar-removal.php
+++ wp-admin-bar-removal.php
@@ -2,17 +2,24 @@
 /*
 Plugin Name: Admin Bar Removal DEVELOPMENTAL
 Plugin URI: http://slangji.wordpress.com/wp-admin-bar-removal/
-Description: Completely <code>Disable</code> new WordPress 3.1+ / 3.2+ / 3.3+ (only) <code>Admin Bar</code> Frontend and Backend. Work under GPLv2 License. Completely <code>Remove Code</code> for minimal memory load. | <a href="http://slangji.wordpress.com/donate/" title="Free Donation">Donate</a> | <a href="http://wordpress.org/extend/plugins/wp-overview-lite/" title="Show Dashboard Overview and Footer Memory Load Usage">WP Overview?</a> | <a href="http://wordpress.org/extend/plugins/wp-missed-schedule/" title="Fix Missed Scheduled Future Posts Cron Job">WP Missed Schedule?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-removal/" title="Remove Admin Bar Frontend Backend User Profile and Code">Admin Bar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-node-removal/" title="Remove Admin Bar Frontend and Backend Node">Admin Bar Node Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-toolbar-removal/" title="Remove ToolBar Frontend Backend User Profile and Code">ToolBar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-toolbar-node-removal/" title="Remove ToolBar Frontend and Backend Node">ToolBar Node Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-login-deindexing/" title="Total DeIndexing WordPress LogIn from all Search Engines">LogIn DeIndexing?</a> | <a href="http://wordpress.org/extend/plugins/wp-total-deindexing/" title="Total DeIndexing WordPress from all Search Engines">WP DeIndexing?</a> | <a href="http://wordpress.org/extend/plugins/wp-ie-enhancer-and-modernizer/" title="Enhancer and Modernizer IE Surfing Expirience">Enhancer IE Surfing?</a>
+Description: Completely <code>Disable</code> new WordPress 3.1+ / 3.2+ / 3.3+ (only) <code>Admin Bar</code> Frontend and Backend. Work under GPLv2 License. Completely <code>Remove Code</code> for minimal memory load. | <a href="http://slangji.wordpress.com/donate/" title="Free Donation">Donate</a> | <a href="http://slangji.wordpress.com/contact/" title="Send Me Bug and Suggestions">Contact</a> | <a href="http://slangji.wordpress.com/themes/" title="sLaNGjI's Custom Themes">My Themes</a> | <a href="http://profiles.wordpress.org/slangji" title="sLaNGjI's Profile @ WordPress.org">My Profile</a> | <a href="http://slangji.wordpress.com/" title="sLaNGjI's WebSite @ WordPress.com">My WordPress</a> | <a href="https://github.com/slangji" title="sLaNGjI's Repositories @ GitHub.com">My GitHub</a> | <a href="http://webscripts.softpedia.com/author/sLa-1869786722.html" title="sLa Developer Page @ SoftPedia.com">My SoftPedia</a> | <a href="http://wordpress.org/extend/plugins/wp-overview-lite/" title="Show Dashboard Overview and Footer Memory Load Usage">WP Overview?</a> | <a href="http://wordpress.org/extend/plugins/wp-missed-schedule/" title="Fix Missed Scheduled Future Posts Cron Job">WP Missed Schedule?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-removal/" title="Remove Admin Bar Frontend Backend User Profile and Code">Admin Bar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-node-removal/" title="Remove Admin Bar Frontend and Backend Node">Admin Bar Node Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-toolbar-removal/" title="Remove ToolBar Frontend Backend User Profile and Code">ToolBar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-toolbar-node-removal/" title="Remove ToolBar Frontend and Backend Node">ToolBar Node Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-login-deindexing/" title="Total DeIndexing WordPress LogIn from all Search Engines">LogIn DeIndexing?</a> | <a href="http://wordpress.org/extend/plugins/wp-total-deindexing/" title="Total DeIndexing WordPress from all Search Engines">WP DeIndexing?</a> | <a href="http://wordpress.org/extend/plugins/wp-ie-enhancer-and-modernizer/" title="Enhancer and Modernizer IE Surfing Expirience">Enhancer IE Surfing?</a> | <a href="http://wordpress.org/extend/plugins/wp-wp-memory-db-indicator/" title="Memory Load Consumption db size Usage Indicator">Memory and db Indicator?</a>
 Version: 2012.0321-BUGFIX.0000-DEVELOPMENTAL
 Author: sLa
 Author URI: http://slangji.wordpress.com/
 Requires at least: 3.1
 Tested up to: 3.3.3
-License: GPLv2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * Major Stable Release: Version 2012 Build 0320 Revision 2012
  *
  * [Admin Bar Removal](http://wordpress.org/extend/plugins/wp-admin-bar-removal/) WordPress PlugIn
+ *
+ * [Contact](http://slangji.wordpress.com/contact/) - [Donate](http://slangji.wordpress.com/donate/)
+ *
+ * [My Plugins](http://slangji.wordpress.com/plugins/) - [My Themes](http://slangji.wordpress.com/themes/)
+ * 
+ * [My GitHub.com](https://github.com/slangji) - [My SoftPedia.com](http://webscripts.softpedia.com/author/sLa-1869786722.html
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
@@ -34,7 +41,9 @@ License: GPLv2
  * wp-admin-bar-node-removal.php by sLa <slangji[at]gmail[dot]com>
  * according to the terms of the GNU General Public License version 2 (or later)
  *
- * Copyright © 2010-2012 [sLa](http://wordpress.org/extend/plugins/profile/slangji) (slangji[at]gmail[dot]com)
+ * Copyright © 2009-2013 [sLaNGjI's](http://profiles.wordpress.org/slangji) (slangji[at]gmail[dot]com)
+ *
+ * Disclaimer: Part of Copyright belongs to [sLa](http://profiles.wordpress.org/sla) and a portion to their respective owners.
  */
 /**
  * @package Admin Bar Removal
@@ -42,7 +51,7 @@ License: GPLv2
  * @since 3.1.0
  * @version 2012.0321-BUGFIX.0000-DEVELOPMENTAL
  * @author sLa
- * @license GPLv2
+ * @license GPLv2 or later
  *
  * Completely Disable Admin Bar Frontend, Backend, and Remove Code for minimal memory load.
  */
@@ -175,4 +184,5 @@ $wp_admin_bar->remove_menu('cloudflare-default');
 $wp_admin_bar->remove_menu('cloudflare-my-websites');
 $wp_admin_bar->remove_menu('cloudflare-analytics');
 $wp_admin_bar->remove_menu('cloudflare-account');
+$wp_admin_bar->remove_menu('wp_memory_db_indicator');
 }?>
